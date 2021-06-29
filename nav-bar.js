ed18577326ab4a0b103cc2681cf99d44bae84f16
Kambu <pereira.hec97@gmail.com>
2021-06-29 12:51:31 -0400

    Linters fix

--- nav-bar.js
+++ nav-bar.js
@@ -1,16 +1,16 @@
-const mainMenu = document.querySelector('.nav-list')
-const openMenu = document.querySelector('.openMenu')
-const closeMenu = document.querySelector('.closeMenu')
-
-openMenu.addEventListener('click', show)
-closeMenu.addEventListener('click', close)
+const mainMenu = document.querySelector('.nav-list');
+const openMenu = document.querySelector('.openMenu');
+const closeMenu = document.querySelector('.closeMenu');
 
 function show(){
-    mainMenu.classList.add('visible')
-    openMenu.style.display = 'none'
+    mainMenu.classList.add('visible');
+    openMenu.style.display = 'none';
 }
 
 function close() {
-    mainMenu.classList.remove('visible')
-    openMenu.style.display = 'flex'
+    mainMenu.classList.remove('visible');
+    openMenu.style.display = 'flex';
 }
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
