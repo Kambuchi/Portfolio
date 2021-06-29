4528f5fffb05c0f7c8c1ba210b3175601b2bfce8
Kambu <pereira.hec97@gmail.com>
2021-06-29 18:33:22 -0400

    Delete main.js file

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-const mainMenu = document.querySelector('.nav-list');
-const openMenu = document.querySelector('.openMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const h = document.querySelector('.h');
-const p = document.querySelector('.p');
-const a = document.querySelector('.a');
-const c = document.querySelector('.c');
--- scripts/nav-bar.js
+++ scripts/nav-bar.js
@@ -1,3 +1,11 @@
+const mainMenu = document.querySelector('.nav-list');
+const openMenu = document.querySelector('.openMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const h = document.querySelector('.h');
+const p = document.querySelector('.p');
+const a = document.querySelector('.a');
+const c = document.querySelector('.c');
+
 function show() {
   mainMenu.classList.add('visible');
   openMenu.style.display = 'none';
