93597f940e8a030f3d3ef305bc539caf74ac32c9
Kambu <pereira.hec97@gmail.com>
2021-06-29 12:53:45 -0400

    Linters fix

--- nav-bar.js
+++ nav-bar.js
@@ -2,14 +2,14 @@ const mainMenu = document.querySelector('.nav-list');
 const openMenu = document.querySelector('.openMenu');
 const closeMenu = document.querySelector('.closeMenu');
 
-function show(){
-    mainMenu.classList.add('visible');
-    openMenu.style.display = 'none';
+function show() {
+  mainMenu.classList.add('visible');
+  openMenu.style.display = 'none';
 }
 
 function close() {
-    mainMenu.classList.remove('visible');
-    openMenu.style.display = 'flex';
+  mainMenu.classList.remove('visible');
+  openMenu.style.display = 'flex';
 }
 
 openMenu.addEventListener('click', show);
